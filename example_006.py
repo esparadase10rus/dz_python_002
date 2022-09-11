2757cd270df0816ca47751f25574da694cb4fc13
Ilya <ackulin.ilya@yandex.ru>
2022-09-11 17:11:24 +0300

    Напишите программу, в которой пользователь будет задавать две строки,

---
+++ example_006.py
@@ -0,0 +1,10 @@
+def search(source: str, research: str) -> int:
+    s_1 = source.lower()
+    s_2 = research.lower()
+    return s_1.count(s_2)
+
+
+if __name__ == '__main__':
+    s1 = input('Введите большую строку: ')
+    s2 = input('Введите искомую строку: ')
+    print(f'Найдено вхождений: {search(s1, s2)}')
